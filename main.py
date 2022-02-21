7c8abe141c736399b7de16c4a3bb5d17a14ec7e8
Istiak Sagar <91744637+istiaksagar@users.noreply.github.com>
2022-02-21 22:25:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -16,44 +16,49 @@ mydatabase="mydb"
 con = pymysql.connect(host="localhost",user="root",password="2642",database="")
 cur = con.cursor()
 
+
 root = Tk()
 root.title("IIUC Central Library")
 root.iconbitmap('logo.ico')
-#img = PhotoImage('logo.png')
-#root.iconphoto(True, img)
-#root.minsize(width=400,height=400)
-#root.maxsize(width=400,height=400)
+bg = Image.open('Library.jpg')
+#bg.thumbnail((600,500))
+newImageSizeWidth, newImageSizeHeight = (600,500)
+img = ImageTk.PhotoImage(bg)
 root.geometry("600x500")
 root.resizable(0,0)
 
 # Take n greater than 0.25 and less than 5
-same=True
-n=0.25
+'''same=False
+n=0.25'''
 
 # Adding a background image
-background_image =Image.open("Lib.jpg")
+'''background_image =Image.open("Lib.jpg")
 [imageSizeWidth, imageSizeHeight] = background_image.size
 
 newImageSizeWidth = int(imageSizeWidth*n)
 if same:
     newImageSizeHeight = int(imageSizeHeight*n) 
 else:
-    newImageSizeHeight = int(imageSizeHeight/n) 
+    newImageSizeHeight = int(imageSizeHeight*n)
     
 background_image = background_image.resize((newImageSizeWidth,newImageSizeHeight),Image.ANTIALIAS)
 img = ImageTk.PhotoImage(background_image)
-
+'''
 Canvas1 = Canvas(root)
 
-Canvas1.create_image(300,340,image = img)      
+Canvas1.create_image(300,340,image = img)
 Canvas1.config(bg="white",width = newImageSizeWidth, height = newImageSizeHeight)
 Canvas1.pack(expand=False,fill=BOTH)
 
-headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
-headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)
+'''headingFrame1 = Frame(root,bg="#FFBB00",bd=5)
+headingFrame1.place(relx=0.2,rely=0.1,relwidth=0.6,relheight=0.16)'''
 
-headingLabel = Label(headingFrame1, text="Welcome to \n IIUC Central Library", bg='black', fg='white', font=('Courier',15))
+'''headingLabel = Label(headingFrame1, text="Welcome to \n IIUC Central Library", bg='black', fg='white', font=('Courier',15))
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
+'''
+Canvas1.create_image(0, 0, image=img, anchor='nw')
+label = Label(root, text="Login Page", height=1, width=9, font=("Georgia 26 bold"), bg='#800000', fg='white', bd=6, relief=RIDGE)
+Canvas1.create_window(180, 40, anchor="nw", window=label)
 
 btn1 = Button(root,text="Add Book Details",bg='black', fg='white', command=addBook)
 btn1.place(relx=0.28,rely=0.3, relwidth=0.45,relheight=0.1)
